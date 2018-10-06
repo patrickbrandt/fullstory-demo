2ce8021ace2473143ed5a1ecc3ea78a83d2c0728
Patrick Brandt <patrickbrandt@Patricks-MacBook-Pro.local>
2018-10-06 17:21:12 -0400

    WIP: portal is working

--- feedback-portal/src/App.js
+++ feedback-portal/src/App.js
@@ -1,24 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FeedbackTable from './FeedbackTable';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feedback: [],
+      loading: true,
+    };
+    this.filters = [];
+    this.feedbackAPI = 'https://mh9x17nwee.execute-api.us-east-1.amazonaws.com/v1/feedback';
+    this.handleFilterSelect = this.handleFilterSelect.bind(this);
+  }
+
+  async handleFilterSelect(e) {
+    const value = e.target.value;
+    if (e.target.checked) {
+      this.filters.push(value);
+    } else {
+      const removeAt = this.filters.findIndex(element => element === value);
+      this.filters.splice(removeAt, 1);
+    }
+
+    const query = `?filter=${this.filters.join(',')}`;
+    const response = await fetch(this.feedbackAPI + query, {
+      method: 'GET'
+    });
+    const feedback = await response.json();
+    this.setState(state => ({feedback}));
+  }
+
+  async componentDidMount() {
+    const response = await fetch(this.feedbackAPI, {
+      method: 'GET'
+    });
+    const feedback = await response.json();
+    this.setState(state => ({feedback}));
+    this.setState(state => (state.loading = false));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Feedback Sentiment</h1>
+          <ul>
+            <li><label><input type="checkbox" id="Positive" value="POSITIVE" onClick={this.handleFilterSelect} />Positive</label></li>
+            <li><label><input type="checkbox" id="Neutral" value="NEUTRAL" onClick={this.handleFilterSelect} />Neutral</label></li>
+            <li><label><input type="checkbox" id="Mixed" value="MIXED" onClick={this.handleFilterSelect} />Mixed</label></li>
+            <li><label><input type="checkbox" id="Negative" value="NEGATIVE" onClick={this.handleFilterSelect} />Negative</label></li>
+            <li><label><input type="checkbox" id="Rage" value="RAGE" onClick={this.handleFilterSelect} />Rage</label></li>
+          </ul>
+          {this.state.feedback.length > 0 && !this.state.loading ? (
+            <FeedbackTable Feedback={this.state.feedback}></FeedbackTable>
+          ) : (
+            <p>You haven't received any feedback yet. Go here and send some: <a href="https://wpb.is/FullStory">https://wpb.is/FullStory</a></p>
+          )}
         </header>
       </div>
     );
---
+++ feedback-portal/src/FeedbackTable.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class FeedbackTable extends Component {
+  render() {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th>Feedback</th>
+              <th>Sentiment</th>
+              <th>Date</th>
+              <th>Replay Link</th>
+            </tr>
+            {this.props.Feedback.map(f =>
+              <tr>
+                <td>{f.feedback}</td>
+                <td>{f.sentiment}</td>
+                <td>{new Date(f.date).toDateString()}, {new Date(f.date).toLocaleTimeString()}</td>
+                <td><a href="{f.sessionURL}">View Session</a></td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default FeedbackTable;
