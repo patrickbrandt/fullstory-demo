c9dc1d476f7191afb7033b2ba148f00e58e222b2
Patrick Brandt <pbrandt@coca-cola.com>
2018-10-08 14:10:06 -0400

    adding in local lint configs

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
 gulp.task('lint', () => {
-  gulp.src(['api/**/*.js', 'feedback-portal/src/*.js', 'homesite/src/**/*.js'])
+  gulp.src(['*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
