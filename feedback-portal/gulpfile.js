571a1b3e58256b147143c50901d224c380f9a1fe
Patrick Brandt <pbrandt@coca-cola.com>
2018-10-08 14:24:09 -0400

    sorting our .elintrc configs and gulpfiles

--- feedback-portal/gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-
-gulp.task('lint', () => {
-  gulp.src(['src/*.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
 gulp.task('lint', () => {
-  gulp.src(['api/**/*.js', 'feedback-portal/src/*.js', 'homesite/src/**/*.js'])
+  gulp.src(['api/**/*.js',
+    'feedback-portal/src/*.js',
+    'homesite/src/**/*.js',
+    '!feedback-portal/src/serviceWorker.js',
+    '!feedback-portal/src/index.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
