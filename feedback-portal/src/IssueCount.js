b050c7e3702340adca256748f17923bcb98df0a3
Patrick Brandt <patrickbrandt@Patricks-MBP.hsd1.ga.comcast.net>
2018-10-09 06:25:01 -0400

    fixing error with component name

--- feedback-portal/src/IssueCount.js
+++ feedback-portal/src/IssueCount.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-class  Emoji extends Component {
+class  IssueCount extends Component {
   render() {
     return (
         <h3 className="issueCount">{this.props.count} issues</h3>
     )};
 }
 
-export default Emoji;
+export default IssueCount;
